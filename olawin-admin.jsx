e7fbb6ce6055b7dd0e62aa18000308f680f1bdd6
Avicohen26 <avianglais@gmail.com>
2026-07-22 11:57:01 +0200

    Update olawin-admin.jsx

--- olawin-admin.jsx
+++ olawin-admin.jsx
@@ -988,7 +988,7 @@ setOrders(snap.docs.map(d => ({ id: d.id, ...d.data() })));
 getDoc(doc(db,"settings","social")).then(snap => { if (snap.exists()) setSocialData(snap.data()); }).catch(()=>{});
 getDoc(doc(db,"settings","content")).then(snap => { if (snap.exists()) setContentData(snap.data()); }).catch(()=>{});
 getDoc(doc(db,"settings","legal")).then(snap => { if (snap.exists()) setLegalData(snap.data()); }).catch(()=>{});
-return () => { unsubD(); unsubO(); };
+getDoc(doc(db,"settings","unsubscribed")).then(snap => { if (snap.exists()) setUnsubList(snap.data().emails||[]); }).catch(()=>{});return () => { unsubD(); unsubO(); };
 }, [authed]);
 
 const saveDraw = async (d) => {
